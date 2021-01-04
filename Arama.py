6620fc58dc133abccff7b4f5c437de18b77026c0
yesill <yesill@localhost.localdomain>
2021-01-04 17:29:07 +0300

    search template downdated

--- Arama.py
+++ Arama.py
@@ -4,19 +4,11 @@ from random import randint
 
 #search_text = ""
 class Arama():
-    #genres = []
     film_list = []
 
     def __init__(self):
         with open('imdb.pkl','rb') as input:
             self.film_list = pickle.load(input)
-        """
-        for i in film_list:
-            genre = i.genre.replace(' ','').split(',')
-            for i in genre:
-                if i not in self.genres:
-                    genres.append(i)
-        """
     
     def searchParametresiz(self):
         """
@@ -64,14 +56,6 @@ class Arama():
         film_list = []
         film_list_same_genre = []
         for film in self.film_list:
-            """
-            if film.name != searched_film.name:
-                film_genres = film.genre.replace(' ','').split(',')
-                genre_counter = 0
-                for genre in searched_film_genres:
-                    if genre in film_genres:
-                        genre_counter += 1
-            """
             #ayni filmi onerme
             if film.name != searched_film.name:
                 if film.genre == searched_film.genre:
